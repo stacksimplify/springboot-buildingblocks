78ab72b86b0c0f666526de172804f7a13b99bd11
Kalyan Reddy Daida <kdaida@Kalyans-MacBook-Pro.local>
2019-07-27 10:08:19 +0530

    Second Commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
