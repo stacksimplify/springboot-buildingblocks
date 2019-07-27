9e25acedfa48598eda2c90e232e80023ee9bb7dc
Kalyan Reddy Daida <kdaida@Kalyans-MacBook-Pro.local>
2019-07-27 09:38:18 +0530

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+
+}
