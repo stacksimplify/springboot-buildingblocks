c9967f6670bca35adeed99208e1f818105aec02f
Kalyan Reddy Daida <kdaida@Kalyans-MacBook-Pro.local>
2019-08-11 20:54:10 +0530

    ContentNegotiation-FirstCommit

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -54,7 +54,7 @@ public class UserController {
 		try {
 			userService.createUser(user);
 			HttpHeaders headers = new HttpHeaders();
-			headers.setLocation(builder.path("/users/{id}").buildAndExpand(user.getId()).toUri());
+			headers.setLocation(builder.path("/users/{id}").buildAndExpand(user.getUserid()).toUri());
 			return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
 			
 		} catch(UserExistsException ex) {
